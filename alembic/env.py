b8d859ead7e4d0dad72d0cdf59ca7a0fab85ebbf
75WAY <75way@75WAYs-Mac-mini.local>
2026-02-27 18:38:54 +0530

    fix issue and add env.example

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,4 @@
-from app.core.config import DATABASE_URL
+from app.core.config import settings
 
 from logging.config import fileConfig
 
@@ -17,7 +17,10 @@ from app.models.progress import Progress
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", DATABASE_URL)
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL
+)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
